6dbb4fc9188dff83a04ca1b39d27e9c51b7a63f6
Paul Painov <paul.parinov@richdataco.com>
2024-04-13 23:24:08 +1000

    adding noice to nvim and RGB to tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -294,7 +294,22 @@ require('lazy').setup({
       },
     },
   },
-
+  -- lazy.nvim
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
   -- NOTE: Plugins can also be configured to run lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
